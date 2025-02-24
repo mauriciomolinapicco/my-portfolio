ae38c7cfd699ba52bdd2100ea6f2ca1be7aa1f38
mauriciomolinapicco <mauriscode@gmail.com>
2025-02-24 18:51:06 -0300

    setup and navbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css"
+import {useState} from 'react'
+import Navbar from './components/Navbar';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+          hola
+        </div>
+
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+    </>
+  )
+}
+
+export default App
